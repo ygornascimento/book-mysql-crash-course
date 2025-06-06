32d736909cbf3933f8f2e66255e4d4a1106d3aa4
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-06-06 15:44:12 -0300

    Criando e excluindo um banco de dados.

---
+++ cap_2.sql
@@ -0,0 +1,11 @@
+show databases;
+
+create database cricus;
+create database finance;
+create database music;
+
+drop database cricus;
+DROP database circus;
+DROP database finance;
+DROP database music;
+
