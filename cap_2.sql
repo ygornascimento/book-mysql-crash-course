84b43a653e8c2693340dd54ccca99992a75dbc25
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-06-06 18:27:22 -0300

    Cap.02 - Primary Keys e Foreign Keys.

--- cap_2.sql
+++ cap_2.sql
@@ -9,3 +9,43 @@ DROP database circus;
 DROP database finance;
 DROP database music;
 
+create database criptocurrency;
+show databases;
+drop database criptocurrency;
+show databases;
+
+create database land;
+
+use land;
+
+create table continent(
+continent_id int,
+continent_name varchar(20),
+population bigint
+);
+show databases;
+
+select * from continent;
+
+create database athletic;
+use athletic;
+
+
+create table sport(
+sport_id int,
+sport_name varchar(50),
+primary key (sport_id)
+);
+
+create table player(
+player_id int,
+player_name varchar(50),
+player_age int,
+sport_id int,
+primary key (player_id),
+foreign key (sport_id) references sport(sport_id)
+);
+
+
+
+
