841d0e043be7e54531c87765fae904bc60ef2138
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-06-07 11:16:17 -0300

    Cap.02 - Check

--- cap_2.sql
+++ cap_2.sql
@@ -56,6 +56,21 @@ primary key(contact_id)
 ); 
 select * from contact;
 
+create table high_temperature(
+city				varchar(50),
+year				int,
+high_temperature	int,
+constraint check (year between 1880 and 2200),
+constraint check (high_temperature < 200),
+primary key (city, year)
+);
+
+select * from high_temperature;
+
+
+
+
+
 
 
 
