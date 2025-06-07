0ac72045acd40f9958d6ff70f1f158e1843a2056
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-06-07 11:55:30 -0300

    Cap.02 - Default

--- cap_2.sql
+++ cap_2.sql
@@ -67,6 +67,16 @@ primary key (city, year)
 
 select * from high_temperature;
 
+create table job(
+job_id			int,
+job_desc		varchar(100),
+shift			varchar(50) default '9-5',
+primary key (job_id)
+);
+
+
+
+
 
 
 
