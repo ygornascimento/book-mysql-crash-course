69937f1c352a7be7fd66832a8dc315fa52271f05
Ygor Nascimento <ygor.nascimento@outlook.com>
2025-06-07 10:13:29 -0300

    Cap.02 - Unique

--- cap_2.sql
+++ cap_2.sql
@@ -51,7 +51,7 @@ contact_id 		int,
 name 			varchar(50) not null,
 city 			varchar(50),
 phone  			varchar(20),
-email_address 	varchar(50),
+email_address 	varchar(50) unique,
 primary key(contact_id)
 ); 
 select * from contact;
